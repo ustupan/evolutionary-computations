d2596bd37f5179bf7d7c4407a5276c9e9cd871c9
LuckyLuke147 <mwojciuk@griddynamics.com>
2020-11-11 16:12:44 +0100

    Scrollable frame with canvas. Print simple plots

--- project1/Gui/gui.py
+++ project1/Gui/gui.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 from tkmacosx import Button
 
 
@@ -7,8 +12,9 @@ class MainApplication(tk.Frame):
     def __init__(self, master):
         self.master = master
         tk.Frame.__init__(self, self.master)
-        self.width = self.master.winfo_screenwidth() / 1.5
-        self.height = self.master.winfo_screenheight() / 1.5
+        self.box = tk.Entry(master)
+        self.width = self.master.winfo_screenwidth() / 1.25
+        self.height = self.master.winfo_screenheight() / 1.25
         self.configure_gui()
         self.create_widgets()
 
@@ -16,31 +22,54 @@ class MainApplication(tk.Frame):
         self.master.title("OE - Projekt nr 1")
         self.master.geometry("%ix%i" % (self.width, self.height))
         self.master.configure(bg="skyblue")
-        self.master.resizable(0, 0)
+        # self.master.resizable(0, 0)
 
     def create_widgets(self):
-        self.create_frames()
+        self.create_default_frames()
         self.create_buttons()
 
-    def create_frames(self):
-        self.left_frame = tk.Frame(width=self.width * 0.65 - 20, height=self.height - 20, background='red')
-        self.left_frame.grid_propagate(0)
-        self.left_frame.grid(row=0, column=0, padx=10, pady=10)
+    def create_default_frames(self):
+        self.left_frame = tk.Frame(width=self.width * 0.65, height=self.height - 25, background='red')
+        self.left_frame.grid(row=0, column=0)
 
         self.right_frame = tk.Frame(width=self.width * 0.35 - 20, height=self.height - 20, background='gold2')
         self.right_frame.grid_propagate(0)
         self.right_frame.grid(row=0, column=1, padx=10, pady=10)
 
+    def create_frames(self):
+        canvas = tk.Canvas(self.left_frame, width=self.width * 0.65, height=self.height - 25, highlightthickness=0,
+                           background='blue')
+        scrollbar = ttk.Scrollbar(self.left_frame, orient="vertical", command=canvas.yview)
+        self.scrollable_frame = ttk.Frame(canvas)
+        # self.scrollable_frame.pack_propagate(0)
+
+        self.left_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        self.plot()
+
+        canvas.create_window((0, 0), window=self.scrollable_frame, anchor=tk.N + tk.W)
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        canvas.pack(side="left", fill=tk.X, expand=True, padx=(10, 0), pady=10)
+        scrollbar.pack(side="left", fill="y", pady=10)
+
+
+
     def create_buttons(self):
-        self.create_left_frame_buttons()
+        # self.create_left_frame_buttons()
         self.create_right_frame_buttons()
 
-    def create_left_frame_buttons(self):
-        self.button1 = Button(self.left_frame, text='Mac OSX', bg='black', fg='green', borderless=1)
-        self.button1.grid(row=0, column=0, padx=30, pady=20)
+    # def create_left_frame_buttons(self):
+    #        self.button1 = Button(self.left_frame, text='Mac OSX', bg='black', fg='green', borderless=1)
+    #      self.button1.grid(row=0, column=0, padx=30, pady=20)
 
-        self.button2 = Button(self.left_frame, text='Button2', borderless=1)
-        self.button2.grid(row=1, column=0, padx=30, pady=20)
+    #  self.button2 = Button(self.left_frame, text='Button2', borderless=1)
+    #  self.button2.grid(row=1, column=0, padx=30, pady=20)
 
     def create_right_frame_buttons(self):
         v = ["one", "two", "three", "four"]
@@ -51,12 +80,36 @@ class MainApplication(tk.Frame):
         self.button1 = Button(self.right_frame, text='Button3', borderless=1)
         self.button1.grid(row=1, column=0)
 
-        self.button2 = Button(self.right_frame, text='Button4', borderless=1)
+        self.button2 = Button(self.right_frame, text='Button4', borderless=1, command=self.create_frames)
         self.button2.grid(row=1, column=1)
 
     def justamethod(self, event):
-        print("method is called")
-        print(self.combo.get())
+        print("method is called: choosen -> ", self.combo.get())
+
+    def plot(self):
+        x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        p = np.array([16.23697, 17.31653, 17.22094, 17.68631, 17.73641, 18.6368, 19.32125, 19.31756, 21.20247, 22.41444,
+                      22.11718, 22.12453])
+
+        fig = Figure(figsize=(6, 6))
+        a = fig.add_subplot(111)
+        a.plot(p, range(2 + max(x)), color='blue')
+
+        a.set_title("Estimation Grid", fontsize=12)
+        a.set_ylabel("Y", fontsize=10)
+        a.set_xlabel("X", fontsize=10)
+
+        canvas0 = FigureCanvasTkAgg(fig, self.scrollable_frame)
+        canvas0.get_tk_widget().pack()
+        canvas0.draw()
+
+        canvas1 = FigureCanvasTkAgg(fig, self.scrollable_frame)
+        canvas1.get_tk_widget().pack()
+        canvas1.draw()
+
+        canvas2 = FigureCanvasTkAgg(fig, self.scrollable_frame)
+        canvas2.get_tk_widget().pack()
+        canvas2.draw()
 
 
 if __name__ == '__main__':
