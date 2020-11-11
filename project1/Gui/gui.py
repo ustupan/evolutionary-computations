af752a1260b239289068b93539e4894e978b45a7
LuckyLuke147 <mwojciuk@griddynamics.com>
2020-11-11 23:34:37 +0100

    Scrollable frame with canvas. Print simple plots

--- project1/Gui/gui.py
+++ project1/Gui/gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.ttk import Frame
 
 import numpy as np
 from matplotlib.figure import Figure
@@ -9,6 +10,8 @@ from tkmacosx import Button
 
 
 class MainApplication(tk.Frame):
+    right_scrollable_frame: Frame
+
     def __init__(self, master):
         self.master = master
         tk.Frame.__init__(self, self.master)
@@ -25,63 +28,76 @@ class MainApplication(tk.Frame):
         # self.master.resizable(0, 0)
 
     def create_widgets(self):
-        self.create_default_frames()
-        self.create_buttons()
-
-    def create_default_frames(self):
-        self.left_frame = tk.Frame(width=self.width * 0.65, height=self.height - 25, background='red')
-        self.left_frame.grid(row=0, column=0)
-
-        self.right_frame = tk.Frame(width=self.width * 0.35 - 20, height=self.height - 20, background='gold2')
-        self.right_frame.grid_propagate(0)
-        self.right_frame.grid(row=0, column=1, padx=10, pady=10)
+        self.create_frames()
 
     def create_frames(self):
-        canvas = tk.Canvas(self.left_frame, width=self.width * 0.65, height=self.height - 25, highlightthickness=0,
-                           background='blue')
-        scrollbar = ttk.Scrollbar(self.left_frame, orient="vertical", command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas)
-        # self.scrollable_frame.pack_propagate(0)
+        self.left_frame = tk.Frame(width=self.width * 0.55, height=self.height - 20, background='red')
+        self.left_frame.grid(row=0, column=0)
+        self.set_canvas_to_left_frame()
 
-        self.left_frame.bind(
+        self.right_frame = tk.Frame(width=self.width * 0.45, height=self.height - 20, background='yellow')
+        self.right_frame.grid(row=0, column=1)
+        self.set_canvas_to_right_frame()
+
+    def set_canvas_to_left_frame(self):
+        left_canvas = tk.Canvas(self.left_frame, width=self.width * 0.55 - 15, height=self.height - 20,
+                                highlightthickness=0,
+                                background="white")
+
+        left_scrollbar = ttk.Scrollbar(self.left_frame, orient="vertical", command=left_canvas.yview)
+        self.left_scrollable_frame = ttk.Frame(left_canvas)
+
+        self.left_scrollable_frame.bind(
             "<Configure>",
-            lambda e: canvas.configure(
-                scrollregion=canvas.bbox("all")
+            lambda e: left_canvas.configure(
+                scrollregion=left_canvas.bbox("all")
             )
         )
+        left_canvas.create_window((0, 0), window=self.left_scrollable_frame, anchor=tk.N + tk.W)
+        left_canvas.configure(yscrollcommand=left_scrollbar.set)
 
-        self.plot()
+        left_canvas.pack(side="left", fill="y", expand=False, pady=10)
+        left_scrollbar.pack(side="right", fill="y", pady=10)
 
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor=tk.N + tk.W)
-        canvas.configure(yscrollcommand=scrollbar.set)
+    def set_canvas_to_right_frame(self):
+        right_canvas = tk.Canvas(self.right_frame, width=self.width * 0.45 - 15, height=self.height - 20,
+                                 highlightthickness=0,
+                                 background="#ECECEC")
 
-        canvas.pack(side="left", fill=tk.X, expand=True, padx=(10, 0), pady=10)
-        scrollbar.pack(side="left", fill="y", pady=10)
+        right_scrollbar = ttk.Scrollbar(self.right_frame, orient="vertical", command=right_canvas.yview)
+        self.right_scrollable_frame = ttk.Frame(right_canvas)
+
+        self.right_scrollable_frame.bind(
+            "<Configure>",
+            lambda e: right_canvas.configure(
+                scrollregion=right_canvas.bbox("all")
+            )
+        )
+        self.create_right_frame_widgets()
+
+        right_canvas.create_window((0, 0), window=self.right_scrollable_frame, anchor=tk.N + tk.W)
+        right_canvas.configure(yscrollcommand=right_scrollbar.set)
+
+        right_canvas.pack(side="left", fill="y", expand=False, pady=10)
+        right_scrollbar.pack(side="right", fill="y", pady=10)
 
 
 
-    def create_buttons(self):
-        # self.create_left_frame_buttons()
-        self.create_right_frame_buttons()
+    def create_right_frame_widgets(self):
+        # v = ["one", "two", "three", "four"]
+        # self.combo = ttk.Combobox(self.right_frame, values=v)
+        # self.combo.grid(row=0, column=0, padx=10, pady=10)
+        # self.combo.bind("<<ComboboxSelected>>", self.justamethod)
+        #
+        # self.button1 = Button(self.right_frame, text='Button3', borderless=1)
+        # self.button1.grid(row=1, column=0)
 
-    # def create_left_frame_buttons(self):
-    #        self.button1 = Button(self.left_frame, text='Mac OSX', bg='black', fg='green', borderless=1)
-    #      self.button1.grid(row=0, column=0, padx=30, pady=20)
+        for i in range (40):
+            self.button2 = ttk.Button(self.right_scrollable_frame, text='Button4', command=self.plot)
+            self.button2.grid(row=i, column=1)
 
-    #  self.button2 = Button(self.left_frame, text='Button2', borderless=1)
-    #  self.button2.grid(row=1, column=0, padx=30, pady=20)
-
-    def create_right_frame_buttons(self):
-        v = ["one", "two", "three", "four"]
-        self.combo = ttk.Combobox(self.right_frame, values=v)
-        self.combo.grid(row=0, column=0, padx=10, pady=10)
-        self.combo.bind("<<ComboboxSelected>>", self.justamethod)
-
-        self.button1 = Button(self.right_frame, text='Button3', borderless=1)
-        self.button1.grid(row=1, column=0)
-
-        self.button2 = Button(self.right_frame, text='Button4', borderless=1, command=self.create_frames)
-        self.button2.grid(row=1, column=1)
+            self.button3 = ttk.Button(self.right_scrollable_frame, text='Button5', command=self.plot)
+            self.button3.grid(row=i, column=2)
 
     def justamethod(self, event):
         print("method is called: choosen -> ", self.combo.get())
@@ -99,16 +115,16 @@ class MainApplication(tk.Frame):
         a.set_ylabel("Y", fontsize=10)
         a.set_xlabel("X", fontsize=10)
 
-        canvas0 = FigureCanvasTkAgg(fig, self.scrollable_frame)
-        canvas0.get_tk_widget().pack()
+        canvas0 = FigureCanvasTkAgg(fig, self.left_scrollable_frame)
+        canvas0.get_tk_widget().grid(column=0, row=0)
         canvas0.draw()
 
-        canvas1 = FigureCanvasTkAgg(fig, self.scrollable_frame)
-        canvas1.get_tk_widget().pack()
+        canvas1 = FigureCanvasTkAgg(fig, self.left_scrollable_frame)
+        canvas1.get_tk_widget().grid(column=0, row=1)
         canvas1.draw()
 
-        canvas2 = FigureCanvasTkAgg(fig, self.scrollable_frame)
-        canvas2.get_tk_widget().pack()
+        canvas2 = FigureCanvasTkAgg(fig, self.left_scrollable_frame)
+        canvas2.get_tk_widget().grid(column=0, row=2)
         canvas2.draw()
 
 
@@ -116,42 +132,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     main_app = MainApplication(root)
     root.mainloop()
-
-# import matplotlib
-# matplotlib.use('TkAgg')
-# import numpy as np
-# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-# from matplotlib.figure import Figure
-# from tkinter import *
-#
-# class mclass:
-#     def __init__(self,  window):
-#         self.window = window
-#         self.box = Entry(window)
-#         self.button = Button (window, text="check", command=self.plot)
-#         self.box.pack ()
-#         self.button.pack()
-#
-#     def plot (self):
-#         x=np.array ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-#         v= np.array ([16,16.31925,17.6394,16.003,17.2861,17.3131,19.1259,18.9694,22.0003,22.81226])
-#         p= np.array ([16.23697,     17.31653,     17.22094,     17.68631,     17.73641 ,    18.6368,
-#             19.32125,     19.31756 ,    21.20247  ,   22.41444   ,  22.11718  ,   22.12453])
-#
-#         fig = Figure(figsize=(6,6))
-#         a = fig.add_subplot(111)
-#         a.scatter(v,x,color='red')
-#         a.plot(p, range(2 +max(x)),color='blue')
-#         a.invert_yaxis()
-#
-#         a.set_title ("Estimation Grid", fontsize=16)
-#         a.set_ylabel("Y", fontsize=14)
-#         a.set_xlabel("X", fontsize=14)
-#
-#         canvas = FigureCanvasTkAgg(fig, master=self.window)
-#         canvas.get_tk_widget().pack()
-#         canvas.draw()
-#
-# window= Tk()
-# start= mclass (window)
-# window.mainloop()
