f76ec93bf05ff6d07d87ff1febad639a8ceba880
andrzej <jodowy@gmail.com>
2020-12-28 17:59:48 +0100

    Podstawowe elementy

--- project3/proj3.py
+++ project3/proj3.py
@@ -12,7 +12,7 @@ def individual(icls, min_val, max_val):
     return icls(genome)
 
 
-def fitness_function(individual):
+def fitness(individual):
     return (1.5 - individual[0] + individual[0] * individual[1]) ** 2 + (
             2.25 - individual[0] + individual[0] * individual[1] ** 2) ** 2 + (
                    2.625 - individual[0] + individual[0] * individual[1] ** 3) ** 2
@@ -28,12 +28,13 @@ def user_input():
     num_of_iter = '0'
 
     while min_max.isnumeric() is False or (0 >= int(min_max) or int(min_max) > 3):
-        min_max = input('Wybierz:\n1. Maksymalizacja \n2. Minializacja \n')
+        min_max = input('Wybierz:\n1. Minializacja \n2. Maksymalizacja \n')
 
     while selection.isnumeric() is False or (0 >= int(selection) or int(selection) > 9):
         selection = input('Selekcja, wybierz:\n1. Turniejowa \n2. Losowa \n3. Najlepszych \n4. Najgorszych \n5. '
                           'Ruletki\n6. Leksykazy\n7. Podwójna Turniejowa\n8. Leksykazy epsilon\n9. Stochastyczna '
                           'uniwersalna')
+
     while crossing.isnumeric() is False or (0 >= int(crossing) or int(crossing) > 6):
         crossing = input(
             'Krzyżowanie, wybierz:\n1. Dwupunktowe \n2. Jednopunktowe \n3. Jednorodne \n4. Częściowo dopasowane \n5. '
@@ -53,6 +54,18 @@ def user_input():
         num_of_iter = input('Liczba iteracji, wybierz:\n')
 
     return min_max, selection, crossing, algorithm, pop_size, prob_mut, prob_cross, num_of_iter
+
+
+def genetics(min_max, min_val, max_val, selection, crossing, algorithm, pop_size, prob_mut, prob_cross, num_of_iter):
+    if min_max == 1:
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    else:
+        creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+    toolbox = base.Toolbox()
+    toolbox.register("individual", individual, creator.Individual, min_val=min_val, max_val=max_val)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("evaluate", fitness)
 
 
 if __name__ == '__main__':
