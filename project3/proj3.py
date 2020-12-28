285a6991d9125c6235b69d97794eb1c89f8f4431
andrzej <jodowy@gmail.com>
2020-12-28 18:31:41 +0100

    Podstawowe elementy + 1

--- project3/proj3.py
+++ project3/proj3.py
@@ -20,12 +20,22 @@ def fitness(individual):
 def user_input():
     min_max = '0'
     selection = '0'
+    tournament_size = '0'
     crossing = '0'
     algorithm = '0'
     pop_size = '0'
     prob_mut = '0'
     prob_cross = '0'
     num_of_iter = '0'
+
+    while prob_mut.isnumeric() is False or (0 >= float(prob_mut) or float(prob_mut) > 1):
+        prob_mut = input('Prawdopodobieństwo mutacji, wybierz:\n')
+
+    while prob_cross.isnumeric() is False or (0 >= float(prob_cross) or float(prob_cross) > 1):
+        prob_cross = input('Prawdopodobieństwo krzyżowania, wybierz:\n')
+
+    while num_of_iter.isnumeric() is False or (0 >= int(num_of_iter) or int(num_of_iter) > 1000):
+        num_of_iter = input('Liczba iteracji, wybierz:\n')
 
     while min_max.isnumeric() is False or (0 >= int(min_max) or int(min_max) > 3):
         min_max = input('Wybierz:\n1. Minializacja \n2. Maksymalizacja \n')
@@ -34,6 +44,10 @@ def user_input():
         selection = input('Selekcja, wybierz:\n1. Turniejowa \n2. Losowa \n3. Najlepszych \n4. Najgorszych \n5. '
                           'Ruletki\n6. Leksykazy\n7. Podwójna Turniejowa\n8. Leksykazy epsilon\n9. Stochastyczna '
                           'uniwersalna')
+
+    if selection == 1:
+        while num_of_iter.isnumeric() is False or (0 >= int(num_of_iter) or int(num_of_iter) > int(pop_size)/2):
+            num_of_iter = input('Wielkość turnieju, wybierz:\n')
 
     while crossing.isnumeric() is False or (0 >= int(crossing) or int(crossing) > 6):
         crossing = input(
@@ -44,14 +58,7 @@ def user_input():
         algorithm = input(
             'Mutacja, wybierz:\n1. Mutacja Gaussa \n2. Tasowanie indeksów\n3. Flip bit \n4. Polynomial bounded \n')
 
-    while prob_mut.isnumeric() is False or (0 >= float(prob_mut) or float(prob_mut) > 1):
-        prob_mut = input('Prawdopodobieństwo mutacji, wybierz:\n')
 
-    while prob_cross.isnumeric() is False or (0 >= float(prob_cross) or float(prob_cross) > 1):
-        prob_cross = input('Prawdopodobieństwo krzyżowania, wybierz:\n')
-
-    while num_of_iter.isnumeric() is False or (0 >= int(num_of_iter) or int(num_of_iter) > 1000):
-        num_of_iter = input('Liczba iteracji, wybierz:\n')
 
     return min_max, selection, crossing, algorithm, pop_size, prob_mut, prob_cross, num_of_iter
 
@@ -67,6 +74,27 @@ def genetics(min_max, min_val, max_val, selection, crossing, algorithm, pop_size
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("evaluate", fitness)
 
+def set_selection(toolbox, selection, k = 0):
+    if selection == 1:
+        pass
+    elif selection == 2:
+        pass
+    elif selection == 3:
+        pass
+    elif selection == 4:
+        pass
+    elif selection == 5:
+        pass
+    elif selection == 6:
+        pass
+    elif selection == 7:
+        pass
+    elif selection == 8:
+        pass
+    elif selection == 9:
+        pass
+
+    return toolbox
 
 if __name__ == '__main__':
     min_max, selection, crossing, algorithm, pop_size, prob_mut, prob_cross, num_of_iter = user_input()
